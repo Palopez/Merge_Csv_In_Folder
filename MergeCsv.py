8695a32a11fadafebb9d369e494d69cfc1cd7f72
Palopez <32514434+Palopez@users.noreply.github.com>
2019-08-23 14:40:56 -0400

    Merge csv Ver1.0
    first version of the python script to merge csv files into a single one

---
+++ MergeCsv.py
@@ -0,0 +1,30 @@
+import glob
+#Modify
+#place the route of the folder where are the csv files
+directory = "C:\yourRoute\FolderContainingTheCsvFiles 
+#Name of the merged csv files
+name = "merged.csv"
+#place the route of destiny to the merged csv
+outputfile = "C:\yourRoute\FolderContainingTheCsvFiles/FolderOfMergedCsv/"+name
+
+print("collecting files...")
+#NO modify
+#Takes all the .csv files of the var directory
+to_merge = glob.glob(directory+"/*.csv")
+to_merge.sort()
+header_saved = False
+inicial = 0;
+with open(outputfile,'w', encoding = 'utf-8-sig') as mergedfile:
+    for filename in to_merge:
+        inicial = inicial + 1;
+        n = inicial;
+        print(str(n)+")"+filename)
+        print("")
+        with open(filename, encoding = 'utf-8-sig') as infiles:
+            header = next(infiles)
+            if not header_saved:
+                mergedfile.write(header)
+                header_saved = True
+            for line in infiles:
+                mergedfile.write(line)
+print("file merged on route "+ directory + "/" + outputfile)
